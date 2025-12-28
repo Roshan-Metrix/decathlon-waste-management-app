360ec788526e6518795d1cc66f35e2772f2177a8
Roshan Patel <roshanpatel12309@gmail.com>
2025-12-28 22:00:48 +0530

    Modified UI

--- frontend/src/Components/Alert.js
+++ frontend/src/Components/Alert.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingVertical: 26,
     paddingHorizontal: 20,
-    // borderRadius: 18,
+    borderRadius: 10,
     elevation: 6,
     shadowColor: "#000",
     shadowOpacity: 0.2,

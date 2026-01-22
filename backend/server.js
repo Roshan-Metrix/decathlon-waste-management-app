1b109ef141f0d3feaa3310b4bf747a51da14ec24
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-22 14:43:23 +0530

    Added vendor route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js'
 import transactionRouter from './routes/transactionRoutes.js';
+import vendorRouter from './routes/vendorRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 const allowedOrigins = [
-  process.env.FRONTEND_URI,
+  'http://ec2-3-109-207-206.ap-south-1.compute.amazonaws.com:3000',
   'http://localhost:5173',
 ];
 
@@ -27,8 +28,9 @@ app.get('/',(req,res) => {
     res.send('API Running...');
 })
 
-app.use('/api/auth',authRouter)
-app.use('/api/manager/transaction',transactionRouter)
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/manager/transaction',transactionRouter)
+app.use('/api/v1/vendor',vendorRouter)
 
 app.listen(port,() => {
     console.log(`Server is running at ${port}`)

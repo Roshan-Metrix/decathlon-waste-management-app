b768cd40fbd7e0b62e85aee2fb9da5ae2c3b61ce
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-23 22:43:58 +0530

    Created timestamp function

---
+++ frontend/src/lib/formatTimeStamp.js
@@ -0,0 +1,11 @@
+export const formatTimeStamp = (timestamp) => {
+    if (!timestamp) return "N/A";
+    const date = new Date(timestamp);
+    return date.toLocaleString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };

dc4523d48b7c02474c764185639b9bc1687966c6
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-29 13:35:05 +0530

    Fixes UI

--- frontend/App.js
+++ frontend/App.js
@@ -38,27 +38,3 @@ export default function App() {
     </InternetProvider>
   );
 }
-// import React from "react";
-// import {
-//   KeyboardAvoidingView,
-//   Platform,
-//   TouchableWithoutFeedback,
-//   Keyboard,
-// } from "react-native";
-// import { AuthProvider } from "./src/context/AuthContext";
-// import AppNavigator from "./src/navigation/AppNavigator";
-
-// export default function App() {
-//   return (
-//     <AuthProvider>
-//       <KeyboardAvoidingView
-//         style={{ flex: 1 }}
-//         behavior={Platform.OS === "ios" ? "padding" : "height"}
-//       >
-//         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-//           <AppNavigator />
-//         </TouchableWithoutFeedback>
-//       </KeyboardAvoidingView>
-//     </AuthProvider>
-//   );
-// }

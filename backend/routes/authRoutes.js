753fe62b813bd032457eded32e71abb0c52feef3
Roshan Patel <roshanpatel12309@gmail.com>
2026-02-23 09:55:09 +0530

    Added delete manager , restrict admin , and edit store route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import {loginUser,logoutUser, getLoggedInUserDetails, sendPasswordResetOtp, resetPassword, registerAdmin, registerStore, registerManager, changePassword } from '../controllers/authController.js';
+import {loginUser,logoutUser, getLoggedInUserDetails, sendPasswordResetOtp, resetPassword, registerAdmin, registerStore, registerManager, changePassword, restrictAnyAdminAccess } from '../controllers/authController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import { getAllAdmins } from '../controllers/adminController.js';
-import { getAllManagers, getManagerProfile, getParticularStoreManagers } from '../controllers/managerController.js';
+import { deleteManager, getAllManagers, getManagerProfile, getParticularStoreManagers } from '../controllers/managerController.js';
 import adminMiddleware from '../middlewares/adminMiddleware.js';
-import { deleteStore, getAllStores, getStoreProfile } from '../controllers/storeController.js';
+import { deleteStore, editStoreDetails, getAllStores, getStoreProfile } from '../controllers/storeController.js';
 import managerMiddleware from '../middlewares/managerMiddleware.js';
 
 const authRouter = express.Router();
@@ -28,7 +28,10 @@ authRouter.post('/admin/registerStore',adminMiddleware,registerStore);
 authRouter.get('/admin/get-all-stores',adminMiddleware,getAllStores);
 authRouter.get('/admin/get-all-admins',authMiddleware,getAllAdmins);
 authRouter.delete('/admin/delete-store/:storeId',adminMiddleware,deleteStore);
+authRouter.delete('/admin/delete-manager/:managerId',adminMiddleware,deleteManager)
 authRouter.get('/get-all-managers',adminMiddleware,getAllManagers);
+authRouter.patch('/admin/restrict-admin/:adminId',adminMiddleware,restrictAnyAdminAccess);
+authRouter.patch('/admin/edit-store/:storeId',adminMiddleware,editStoreDetails)
 
 //manager
 authRouter.post('/registerManager',authMiddleware,registerManager);

37fbfdfa222e9282bf3543e018ba103090d2677e
Roshan Patel <roshanpatel12309@gmail.com>
2026-06-06 11:56:04 +0545

    Added cc

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -20,12 +20,9 @@ const normalizeSmtpUser = (smtpUser, senderEmail) => {
   return smtpUser;
 };
 
-const mailSender = "decathlonwms1";
-const mailUser = "decathlonwms1@gmail.com";
-
 const smtpUser = normalizeSmtpUser(
-  mailSender,
-  mailUser
+  process.env.SMTP_USER,
+  process.env.SENDER_EMAIL
 );
 
 const transporter = nodemailer.createTransport({
@@ -34,51 +31,9 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: "decathlonwms1",
-    pass: "cohruykvpvongqln",
+    user: smtpUser,
+    pass: process.env.SMTP_PASS,
   },
 });
 
 export default transporter;
-
-
-
-// import nodemailer from "nodemailer";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const normalizeSmtpUser = (smtpUser, senderEmail) => {
-//   if (!smtpUser) {
-//     return senderEmail;
-//   }
-
-//   if (smtpUser.includes("@")) {
-//     return smtpUser;
-//   }
-
-//   if (senderEmail?.includes("@")) {
-//     const [, domain] = senderEmail.split("@");
-//     return `${smtpUser}@${domain}`;
-//   }
-
-//   return smtpUser;
-// };
-
-// const smtpUser = normalizeSmtpUser(
-//   process.env.SMTP_USER,
-//   process.env.SENDER_EMAIL
-// );
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: smtpUser,
-//     pass: process.env.SMTP_PASS,
-//   },
-// });
-
-// export default transporter;
